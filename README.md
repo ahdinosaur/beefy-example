5adbee4c687dfad6691acf19bed00892e8f00b9e
Michael Williams <dinosaur@riseup.net>
2014-10-09 01:51:39 -0700

    Updates

---
+++ README.md
@@ -0,0 +1,30 @@
+# example
+
+this is an example
+
+## how
+
+### install
+
+```
+git clone https://github.com/ahdinosaur/beefy-example
+npm install
+```
+
+## develop
+
+```
+npm start
+```
+
+## test
+
+```
+npm test
+```
+
+## deploy
+
+```
+npm run deploy
+```
---
+++ bundle.js
@@ -0,0 +1,7 @@
+(function e(t,n,r){function s(o,u){if(!n[o]){if(!t[o]){var a=typeof require=="function"&&require;if(!u&&a)return a(o,!0);if(i)return i(o,!0);var f=new Error("Cannot find module '"+o+"'");throw f.code="MODULE_NOT_FOUND",f}var l=n[o]={exports:{}};t[o][0].call(l.exports,function(e){var n=t[o][1][e];return s(n?n:e)},l,l.exports,e,t,n,r)}return n[o].exports}var i=typeof require=="function"&&require;for(var o=0;o<r.length;o++)s(r[o]);return s})({1:[function(require,module,exports){
+var css="main {\n  text-align: center;\n}";require("/home/michael/repos/ahdinosaur/beefy-example/node_modules/cssify")(css),module.exports=css;
+},{"/home/michael/repos/ahdinosaur/beefy-example/node_modules/cssify":3}],2:[function(require,module,exports){
+require("./index.css"),document.querySelector("main").textContent="this is an example";
+},{"./index.css":1}],3:[function(require,module,exports){
+module.exports=function(e,t){var r=t||document;if(r.createStyleSheet){var n=r.createStyleSheet();return n.cssText=e,n.ownerNode}var a=r.getElementsByTagName("head")[0],l=r.createElement("style");return l.type="text/css",l.styleSheet?l.styleSheet.cssText=e:l.appendChild(r.createTextNode(e)),a.appendChild(l),l},module.exports.byUrl=function(e){if(document.createStyleSheet)return document.createStyleSheet(e).ownerNode;var t=document.getElementsByTagName("head")[0],r=document.createElement("link");return r.rel="stylesheet",r.href=e,t.appendChild(r),r};
+},{}]},{},[2]);
